7e6acf98471cc4d7e01714cb0a34631cd02212fd
Facundo Pellicer <facundopellicer4@gmail.com>
2023-03-07 21:06:15 -0300

    disable no-null-assertion eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ const config = {
       {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
+        "@typescript-eslint/no-non-null-assertion": "off",
       },
     ],
   },
